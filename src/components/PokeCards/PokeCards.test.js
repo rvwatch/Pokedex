734574e186a36e509b51b7b1f5fddb00c418ddee
Ricardo Viera <ricardoclean@gmail.com>
2018-02-22 13:31:03 -0700

    Add tests for components and
    begin test for Container

--- src/components/PokeCards/PokeCards.test.js
+++ src/components/PokeCards/PokeCards.test.js
@@ -1,5 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import PokeCards from './PokeCards';
+
 describe('PokeCards component', () => {
-  it('should', () => {
-    
+
+  it('should render correctly', () => {
+    const wrapper = shallow(<PokeCards />);
+    expect(wrapper).toMatchSnapshot();
   });
 });
